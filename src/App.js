d849219843f50eb6086ea5ef6e313e3898f6e2ca
Morgan Blem <morgan.blem@gmail.com>
2019-12-09 14:26:30 +0200

    set up git ignore properly

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import './App.css'
 class App extends Component {
   state = {
     ninjas: [
-      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
-      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
-      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+      { name: 'Morgan', age: 20, belt: 'black', id: 1 },
+      { name: 'Chesney', age: 20, belt: 'orange', id: 2 },
+      { name: 'Jordan', age: 21, belt: 'brown', id: 3 }
     ]
   }
   addNinja = (ninja) => {
@@ -26,6 +26,13 @@ class App extends Component {
     this.setState({
       ninjas: ninjas
     });
+  }
+  componentDidMount(){
+    console.log('component mounted');
+  }
+  componentDidUpdate(prevProps, prevState, snapshot){
+    console.log('component updated');
+    console.log(prevProps, prevState);
   }
   render() {
     return (
