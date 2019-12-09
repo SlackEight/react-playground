db3a90a97dac06f506700a36dcc65dfd89ebe82c
Morgan Blem <morgan.blem@gmail.com>
2019-12-09 14:53:35 +0200

    started poketimes

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <div className="container">
+        <h4 className="center">Contact</h4>
+        <p>stuffssadsad</p>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="container">
+        <h4 className="center">Home</h4>
+        <p>reeeeEEEE33eeEE</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
